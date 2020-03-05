d783db903b31c4ee726c4239821b2035e35d51de
rainy-me <github@rainy.me>
2020-03-06 02:24:28 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img class="m-auto m-0" alt="Vue logo" src="./assets/logo.png" />
+    <div class="bg-gray-200 p-4">
+      <span class="block text-gray-700 text-center bg-gray-400 px-4 py-2">1</span>
+      <span class="block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-2">2</span>
+      <span class="block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-2">3</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {}
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
